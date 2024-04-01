44bf8196f1197b6da6e1c567f725845a6e886d19
David R_ <36084356+infodavide@users.noreply.github.com>
2024-04-01 11:07:33 +0200

    Documentation and fix on description to use plain text.

--- setup.py
+++ setup.py
@@ -2,17 +2,21 @@
 """
 Learn more: https://github.com/infodavide/enedis_data_connect
 """
+import markdown # pip install markdown
+from bs4 import BeautifulSoup
 from setuptools import setup, find_packages
 
 with open('README.md', encoding='utf-8') as f:
-    readme_content = f.read()
+    html = markdown.markdown(f.read())
+    soup = BeautifulSoup(html, features='html.parser')
+    readme_content = soup.get_text()
 
 with open('LICENSE', encoding='utf-8') as f:
     license_content = f.read()
 
 setup(
     name='enedis_data_connect',
-    version='1.0.0',
+    version='1.0.1',
     description='Enedis data connect client',
     long_description=readme_content,
     author='David R', 
